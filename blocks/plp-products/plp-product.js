d551b31935e4086a3301f7c57b9a522154ab8fbb
Chandra shekhar Gupta <chandragupta@adobe.com>
2025-03-23 19:02:36 +0530

    plp products

---
+++ blocks/plp-products/plp-product.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+    block.classList.add('plp-static');
+  
+    const title = document.createElement('h2');
+    title.className = 'plp-title';
+    title.textContent = block.dataset.title || 'Choose what best suits you';
+    block.prepend(title);
+  }
+  
