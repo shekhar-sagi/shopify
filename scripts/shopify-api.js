04a767cc8daaaa86eeb3553f0863f9f1f9691e14
Chandra shekhar Gupta <chandragupta@adobe.com>
2025-03-25 01:01:47 +0530

    code refactor

--- scripts/shopify-api.js
+++ scripts/shopify-api.js
@@ -1,6 +1,5 @@
-
-const SHOPIFY_STORE_DOMAIN = "y5kch8-7n.myshopify.com";
-const SHOPIFY_ACCESS_TOKEN = "6964502587c1f9d114a7e3f93fd0a40b";
+const SHOPIFY_STORE_DOMAIN = 'y5kch8-7n.myshopify.com';
+const SHOPIFY_ACCESS_TOKEN = '6964502587c1f9d114a7e3f93fd0a40b';
 
 const PRODUCTS_QUERY = `
   {
@@ -31,20 +30,22 @@ const PRODUCTS_QUERY = `
   }
 `;
 
-export async function fetchShopifyProducts() {
+async function fetchShopifyProducts() {
   const response = await fetch(
     `https://${SHOPIFY_STORE_DOMAIN}/api/2025-01/graphql.json`,
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "X-Shopify-Storefront-Access-Token": SHOPIFY_ACCESS_TOKEN,
-        "Content-Type": "application/json",
-        Accept: "application/json",
+        'X-Shopify-Storefront-Access-Token': SHOPIFY_ACCESS_TOKEN,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
       },
       body: JSON.stringify({ query: PRODUCTS_QUERY }),
-    }
+    },
   );
 
   const json = await response.json();
   return json?.data?.products?.edges?.map((edge) => edge.node) || [];
 }
+
+export default fetchShopifyProducts;
