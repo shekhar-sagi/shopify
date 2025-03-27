9263b7059e96e4ccaf8560d783c8965e38ce8348
Chandra shekhar Gupta <chandragupta@adobe.com>
2025-03-28 02:04:25 +0530

    eslint fixess

--- scripts/shopify-api.js
+++ scripts/shopify-api.js
@@ -1,5 +1,5 @@
-const SHOPIFY_STORE_DOMAIN = 'y5kch8-7n.myshopify.com';
-const SHOPIFY_ACCESS_TOKEN = '6964502587c1f9d114a7e3f93fd0a40b';
+const SHOPIFY_STORE_DOMAIN = "y5kch8-7n.myshopify.com";
+const SHOPIFY_ACCESS_TOKEN = "6964502587c1f9d114a7e3f93fd0a40b";
 
 const PRODUCTS_QUERY = `
   {
@@ -34,14 +34,14 @@ async function fetchShopifyProducts() {
   const response = await fetch(
     `https://${SHOPIFY_STORE_DOMAIN}/api/2025-01/graphql.json`,
     {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'X-Shopify-Storefront-Access-Token': SHOPIFY_ACCESS_TOKEN,
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
+        "X-Shopify-Storefront-Access-Token": SHOPIFY_ACCESS_TOKEN,
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
       body: JSON.stringify({ query: PRODUCTS_QUERY }),
-    },
+    }
   );
 
   const json = await response.json();
@@ -49,64 +49,3 @@ async function fetchShopifyProducts() {
 }
 
 export default fetchShopifyProducts;
-
-async function fetchShopifyProductById(productId) {
-  const PRODUCT_QUERY = `
-    query ($id: ID!) {
-      product(id: $id) {
-        id
-        title
-        descriptionHtml
-        featuredImage {
-          url
-          altText
-        }
-        images(first: 5) {
-          edges {
-            node {
-              url
-              altText
-            }
-          }
-        }
-        variants(first: 10) {
-          edges {
-            node {
-              id
-              title
-              availableForSale
-              priceV2 {
-                amount
-                currencyCode
-              }
-              selectedOptions {
-                name
-                value
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  const variables = { id: productId };
-
-  const response = await fetch(
-    `https://${SHOPIFY_STORE_DOMAIN}/api/2025-01/graphql.json`,
-    {
-      method: 'POST',
-      headers: {
-        'X-Shopify-Storefront-Access-Token': SHOPIFY_ACCESS_TOKEN,
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({ query: PRODUCT_QUERY, variables }),
-    }
-  );
-
-  const json = await response.json();
-  return json?.data?.product || null;
-}
-
-export default fetchShopifyProductById;
