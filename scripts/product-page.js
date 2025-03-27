7bcdb7b5e0394652c7822dbdb268821b5121b0b1
Chandra shekhar Gupta <chandragupta@adobe.com>
2025-03-28 02:05:23 +0530

    eslint fixess

--- scripts/product-page.js
+++ scripts/product-page.js
@@ -1,5 +1,5 @@
-const SHOPIFY_STORE_DOMAIN = "y5kch8-7n.myshopify.com";
-const SHOPIFY_ACCESS_TOKEN = "6964502587c1f9d114a7e3f93fd0a40b";
+const SHOPIFY_STORE_DOMAIN = 'y5kch8-7n.myshopify.com';
+const SHOPIFY_ACCESS_TOKEN = '6964502587c1f9d114a7e3f93fd0a40b';
 
 async function fetchShopifyProductById(productId) {
   const PRODUCT_QUERY = `
@@ -46,11 +46,11 @@ async function fetchShopifyProductById(productId) {
   const response = await fetch(
     `https://${SHOPIFY_STORE_DOMAIN}/api/2025-01/graphql.json`,
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "X-Shopify-Storefront-Access-Token": SHOPIFY_ACCESS_TOKEN,
-        "Content-Type": "application/json",
-        Accept: "application/json",
+        'X-Shopify-Storefront-Access-Token': SHOPIFY_ACCESS_TOKEN,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
       },
       body: JSON.stringify({ query: PRODUCT_QUERY, variables }),
     }
