7877d34754f997b749f7ba3c5cda4f76597865fe
Roobalakshmi15 <roobalakshmi@adobe.com>
2025-04-03 11:00:05 +0530

    checkout page changes

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -3,35 +3,99 @@
  * Recreate a table
  * https://www.hlx.live/developer/block-collection/table
  */
+import fetchCartDetails from '../../scripts/cart-page.js';
 
-function buildCell(rowIndex) {
-  const cell = rowIndex ? document.createElement('td') : document.createElement('th');
-  if (!rowIndex) cell.setAttribute('scope', 'col');
-  return cell;
-}
 export default async function decorate(block) {
+  console.log('Table block');
+  const productCard = document.createElement('div');
+  productCard.className = 'product-card';
+  const productCardCol1 = document.createElement('div');
+  productCardCol1.className = 'col1';
+  const productCardCol2 = document.createElement('div');
+  productCardCol2.className = 'col2';
+
+  const productImg = document.createElement('img');
+  productCardCol1.append(productImg);
+  const productDetailTemplate = '<div>title</div><div>price</div><div>description</div>';
+  productCardCol2.innerHTML = productDetailTemplate;
+  productCard.append(productCardCol1, productCardCol2);
+  const cartId = 'gid://shopify/Cart/Z2NwLWFzaWEtc291dGhlYXN0MTowMUpROFgxRTJEV0E5VjIxVlFESEVHQlRBMg'; // Replace with actual product I
+
+  // call to fetch cart
+  try {
+    console.log(cartId);
+    const cart = await fetchCartDetails(cartId);
+    console.log(cart);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Shopify API Error:', err);
+  }
+
   const table = document.createElement('table');
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
 
-  const header = !block.classList.contains('no-header');
-  if (header) table.append(thead);
+  table.append(thead);
   table.append(tbody);
+  const tabl = {
+    head: {
+      headIndex: 0,
+      cells: [{
+        cellIndex: 0,
+        cellValue: 'PRODUCT',
+      },
+      {
+        cellIndex: 1,
+        cellValue: 'QUANTITY',
+      },
+      {
+        cellIndex: 2,
+        cellValue: 'TOTAL',
+      }],
+    },
+    row: [{
+      rowIndex: 0,
+      cells: [{
+        cellIndex: 0,
+        cellValue: 'cell 1',
+      },
+      {
+        cellIndex: 0,
+        cellValue: 'cell 2',
+      },
+      {
+        cellIndex: 0,
+        cellValue: 'cell 3',
+      }],
+    }],
+  };
+  console.log(tabl.row);
 
-  [...block.children].forEach((child, i) => {
-    const row = document.createElement('tr');
-    if (header && i === 0) thead.append(row);
-    else tbody.append(row);
-    [...child.children].forEach((col) => {
-      const cell = buildCell(header ? i : i + 1);
-      const align = col.getAttribute('data-align');
-      const valign = col.getAttribute('data-valign');
-      if (align) cell.style.textAlign = align;
-      if (valign) cell.style.verticalAlign = valign;
-      cell.innerHTML = col.innerHTML;
-      row.append(cell);
+  tabl.head.cells.forEach((cell) => {
+    console.log(cell);
+    const tcell = document.createElement('th');
+    tcell.textContent = cell.cellValue;
+    thead.append(tcell);
+  });
+
+  tabl.row.forEach((row1) => {
+    const trow = document.createElement('tr');
+    tbody.append(trow);
+    console.log(row1.cells);
+    row1.cells.forEach((cell, cellIndex) => {
+      const tcell = document.createElement('td');
+      if (cellIndex === 0) {
+        tcell.append(productCard);
+        trow.append(tcell);
+      } else if (cellIndex === 2) {
+        tcell.textContent = '$100';
+      } else {
+        tcell.textContent = 'counter';
+      }
+      trow.append(tcell);
     });
   });
+
   block.innerHTML = '';
   block.append(table);
 }
---
+++ scripts/cart-page.js
@@ -0,0 +1,50 @@
+const SHOPIFY_STORE_DOMAIN = 'y5kch8-7n.myshopify.com';
+const SHOPIFY_ACCESS_TOKEN = '6964502587c1f9d114a7e3f93fd0a40b'; // 🔐 Consider using an environment variable
+async function fetchCartDetails() {
+  const CART_QUERY = `query getCart($cartId: ID!) {
+        cart(id: $cartId) {
+          id
+          checkoutUrl
+          totalQuantity
+          lines(first: 5) {
+            edges {
+              node {
+                id
+                quantity
+                merchandise {
+                  ... on ProductVariant {
+                    id
+                    title
+                    priceV2 {
+                      amount
+                      currencyCode
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }`;
+
+  const variables = {
+    cartId: 'gid://shopify/Cart/NEW_CART_ID',
+  };
+  const response = await fetch(
+    `https://${SHOPIFY_STORE_DOMAIN}/api/2025-01/graphql.json`,
+    {
+      method: 'POST',
+      headers: {
+        'X-Shopify-Storefront-Access-Token': SHOPIFY_ACCESS_TOKEN,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ query: CART_QUERY, variables }),
+    },
+  );
+
+  const json = await response.json();
+  return json?.data?.products?.edges?.map((edge) => edge.node) || [];
+}
+
+export default fetchCartDetails;
